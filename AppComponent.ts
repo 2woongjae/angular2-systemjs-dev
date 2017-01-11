af751b288c234bdc6eebde229a0e31d06d7ab6d9
mark <mark@protopie.io>
2017-01-12 02:56:07 +0900

    init

---
+++ AppComponent.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app',
+    template: `
+    <h2>App Start</h2>
+`
+})
+export class AppComponent {}
---
+++ systemjs.config.js
@@ -0,0 +1,17 @@
+(function (global) {
+    System.config({
+        defaultJSExtensions: true,
+        paths: {
+            "npm:": "node_modules/"
+        },
+        map: {
+            "@angular/core": "npm:@angular/core/bundles/core.umd.js",
+            "@angular/common": "npm:@angular/common/bundles/common.umd.js",
+            "@angular/compiler": "npm:@angular/compiler/bundles/compiler.umd.js",
+            "@angular/platform-browser": "npm:@angular/platform-browser/bundles/platform-browser.umd.js",
+            "@angular/platform-browser-dynamic": "npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js",
+
+            "rxjs": "npm:rxjs"
+        }
+    });
+})(this);
